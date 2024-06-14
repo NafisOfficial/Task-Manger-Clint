909c6f2951927c61343dc1222301ca4e0db17265
Md Nafis Iqbal <nafisiqbal.net2002@gmail.com>
2024-06-14 12:36:02 +0600

    Set auth provider

--- src/Providers/AuthProviders/AuthProviders.jsx
+++ src/Providers/AuthProviders/AuthProviders.jsx
@@ -1,19 +1,62 @@
-import React, { createContext } from 'react';
+import { createContext, useEffect, useState } from "react";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
+import { app } from "../Firebase/Firebase.config";
 
-export const AuthContext = createContext(null)
-
-const AuthProviders = ( {children} ) => {
+export const AuthContex = createContext(null)
 
 
+
+
+const AuthProvider = ( {children} ) => {
+    const auth = getAuth(app);
+    const [loading,isLoading] = useState(true)
+    const [user,setUser] = useState(null)
+
+    const handleLogin=(email,password)=>{
+        return signInWithEmailAndPassword(auth,email,password)
+    }
+
+    const handleSignUp=(email,password)=>{
+        return createUserWithEmailAndPassword(auth,email,password)
+    }
+
+    const handleSignUpByGoogle=(provider)=>{
+        return signInWithPopup(auth,provider)
+    }
+
+    const handleLogOut=()=>{
+        return signOut(auth);
+    }
+
+    //get the user
+    useEffect(()=>{
+        const unSubscribe = onAuthStateChanged(auth,loggedUser=>{
+            setUser(loggedUser);
+            isLoading(false);
+        })
+
+        return ()=>{
+            unSubscribe();
+        }
+    },[])
 
     const AuthInfo = {
-        
+        auth,
+        loading,
+        isLoading,
+        user,
+        setUser,
+        handleLogin,
+        handleSignUp,
+        handleSignUpByGoogle,
+        handleLogOut
     }
-    return ( 
-        <AuthContext.Provider value={AuthInfo}>
+
+    return (
+        <AuthContex.Provider value={AuthInfo}>
             {children}
-        </AuthContext.Provider>
+        </AuthContex.Provider>
     );
 };
 
-export default AuthProviders;
+export default AuthProvider;
